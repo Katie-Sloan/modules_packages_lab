f2ed41be7e864addbf924697d2f2496f5a368ce2
Rob <rob@robs-imac.home>
2020-10-15 16:52:08 +0100

    Initial commit

---
+++ start_code/modules/output.py
@@ -0,0 +1,27 @@
+def print_task(task):
+    print(f'Description: { task["description"] }')
+    print(f'Status: { "Completed" if task["completed"] else "Incomplete"}')
+    print(f'Time Taken: {task["time_taken"]} mins')
+
+def print_list(list):
+    for task in list:
+        print_task(task)
+
+def print_task_descriptions(list):
+    for task in list:
+        print(task["description"])
+
+def get_uncompleted_tasks(list):
+    uncompleted_tasks = []
+    for task in list:
+        if task["completed"] == False:
+            uncompleted_tasks.append(task)
+    return uncompleted_tasks
+
+## Get list of completed tasks
+def get_completed_tasks(list):
+    completed_tasks = []
+    for task in list:
+        if task["completed"] == True:
+            completed_tasks.append(task)
+    return completed_tasks
